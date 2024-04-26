76de84b7150d5089c522568157374aa767160210
lee-dias <leedias.dev@gmail.com>
2024-04-26 11:32:18 +0100

    program cria um menu e os dois players iniciais

--- PlayerManager1/Program.cs
+++ PlayerManager1/Program.cs
@@ -1,12 +1,75 @@
-﻿using System;
+﻿﻿using System;
+using System.Collections.Generic;
 
-namespace PlayerManager1
+namespace PlayerManager1 
 {
-    class Program
+
+    public class Program
     {
-        static void Main(string[] args)
+
+        private List<Player> playerList;
+
+
+        private static void Main()
         {
-            Console.WriteLine("Hello, World!");
+
+            Program prog = new Program();
+
+            prog.Start();
         }
+
+
+        private Program()
+        {
+
+            playerList = new List<Player>() {
+                new Player("Kendrick", 972),
+                new Player("Future", 230)
+            };
+        }
+
+        private void Start()
+        {
+            string option;
+            do
+            {
+                Console.WriteLine("Choose what u want to do:");
+                Console.WriteLine("1- Insert Players & Scores");
+                Console.WriteLine("2- Show Scores");
+                Console.WriteLine("3- Players with higher scores than");
+                Console.WriteLine("4- Leave the program");
+                option = Console.ReadLine();
+
+                
+                switch (option)
+                {
+                    case "1":
+                        
+                        break;
+                    case "2":
+                        
+                        break;
+                    case "3":
+                        
+                        break;
+                    case "4":
+                        
+                        break;
+                    default:
+                        Console.Error.WriteLine("\n>>> Unknown option! <<<\n");
+                        break;
+                }
+
+                
+                Console.Write("\nPress any key to continue...");
+                Console.Read();
+                Console.WriteLine("\n");
+
+                
+            } while (option != "4");
+        }
+
+
+ 
     }
 }
